afb9320be25b1233d8dcea5998e389b61f654c94
claire <clhsu@mit.edu>
2017-11-12 01:16:35 -0500

    commented code

--- lab.py
+++ lab.py
@@ -14,6 +14,10 @@ class EvaluationError(Exception):
     pass
 
 class Environment():
+    """
+    Environment class: contains the variables and the pointer to the parent
+    user can define variables and retrieve variables
+    """
     def __init__(self,parent = carlae_builtins):
         self.variables = {}
         self.parent_environment = parent
@@ -22,6 +26,12 @@ class Environment():
     def define(self,var,value):
         self.variables[var] = value
     def retrieve(self,variable):
+        """
+        Retrieving will depend on whether the variable is in this environment
+        or the parents.
+        :param variable: variable to look up
+        :return: the variable value, or if None, raise an error
+        """
         k = self.variables.get(variable)
         if k == None:
             if self.parent_environment == None:
@@ -29,7 +39,6 @@ class Environment():
             elif self.parent_environment == carlae_builtins:
                 p = carlae_builtins.get(variable)
                 if p == None:
-                    print("Error",variable,self.parent_environment)
                     raise EvaluationError
                 return p
             else:
@@ -37,11 +46,21 @@ class Environment():
         return k
 
 class func():
+    """
+    A class structure to represent functions. Contains the function body (ie the
+    series of commands for the function and the variable names.
+    """
     def __init__(self,variable_names,body,parent):
         self.vars = variable_names
         self.body = body
         self.env_parent = parent
     def execute(self,variables):
+        """
+        Execute the function given a list of inputs
+        Create an environment to execute code body in
+        :param variables: variables to be mapped to the variable names
+        :return: evaluate the body with this new function environment
+        """
         if len(variables) != len(self.vars):
             raise EvaluationError
         new_env = Environment(parent=self.env_parent)
@@ -64,6 +83,7 @@ def tokenize(source):
     return_list=[]
     current_word = ''
     ignore = False
+    # track character by character, checking if semicolons, spaces, or newlines pop up
     for character in source:
         if ignore and character!='\n':
             continue
@@ -100,6 +120,7 @@ def parse(tokens):
         tokens (list): a list of strings representing tokens
     """
 
+    # basically the recursive call for parse to create lists within lists
     def listify(token_list):
         i = 0
         current_list = []
@@ -117,6 +138,7 @@ def parse(tokens):
                     current_list.append(token_list[i])
                 token_list = token_list[1:]
 
+    # make sure your parantehse are matched
     if not paran_count(tokens):
         raise SyntaxError
 
@@ -129,6 +151,7 @@ def parse(tokens):
         else:
             return tokens[0]
 
+# check to see if something can be a float
 def numb(k):
     try:
         float(k)
@@ -136,6 +159,7 @@ def numb(k):
     except ValueError:
         return False
 
+# check to see if your paranthese are matched
 def paran_count(l):
     left = 0
     for token in l:
@@ -154,6 +178,7 @@ def divide(l):
     for item in l[1:]:
         j = j/item
     return j
+
 def multiply(l):
     j = l[0]
     for item in l[1:]:
@@ -170,43 +195,64 @@ def evaluate(tree,env = Environment()):
         tree (type varies): a fully parsed expression, as the output from the
                             parse function
     """
+
+    # basically the recursive call for evaluate.
     def recur(current_exp):
+
+        #an empty input
         if current_exp == []:
             raise EvaluationError
+
+        # if youre defining a variable
         if current_exp[0] == "define":
 
             if type(current_exp[1]) != list:
                 f = evaluate(current_exp[2], env)
                 env.define(current_exp[1],f)
                 return f
+
+            # if youre defining a function lambda
             else:
                 p = func(current_exp[1][1:],current_exp[2],Environment(parent=env))
                 env.define(current_exp[1][0],p)
                 return p
+
+        # if youre trying to do a function
         elif current_exp[0] == "lambda":
             p = func(current_exp[1],current_exp[2],Environment(parent=env))
             return p
+
         current_function = None
+
+        # if youre trying to retrieve some sort of function
         if type(current_exp[0])!=list:
             current_function = env.retrieve(current_exp[0])
-
-        if current_function == None and type(current_exp[0]) != list:
-            raise EvaluationError
+            if current_function == None:
+                raise EvaluationError
+        # if you have to evaluate the list first
         elif type(current_exp[0]) == list:
             current_function = evaluate(current_exp[0],env)
+
+        # now that you have your function, retrieve ur inputs
         input_values = []
         for value in current_exp[1:]:
             if type(value) != list:
+                # if your input is a number
                 if type(value) == float or type(value) == int:
                     input_values.append(value)
+                # if your input is a variable
                 else:
                     v = env.retrieve(value)
                     input_values.append(v)
+            # if your input is a list and needs to be eval'd
             else:
                 input_values.append(recur(value))
+        # now execute ur function (if its a user defined function, otherwise just run it)
         if type(current_function)==func:
             return current_function.execute(input_values)
         return current_function(input_values)
+
+    # if your input isnt a list and you just wanna retrieve a variable or return the tree
     if type(tree) == str:
         v = env.retrieve(tree)
         if v != None:
@@ -215,7 +261,9 @@ def evaluate(tree,env = Environment()):
         return tree
     return recur(tree)
 
-def result_and_env(tree,env=Environment()):
+def result_and_env(tree,env=None):
+    if env==None:
+        env = Environment()
     r = evaluate(tree,env)
     return r,env
 
